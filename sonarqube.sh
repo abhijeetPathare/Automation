cc0310015cde9bbec078428b15255dceb00e658d
Abhijeet Pathare <abhipathare24111988@gmail.com>
2024-08-17 18:27:12 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -3,4 +3,4 @@
 # Update the package index
 sudo apt update
 sudo apt install docker.io -y
-sudo docker run -d -p 8081:8081 sonarqube:lts-community
+sudo docker run -d -p 9000:9000 sonarqube:lts-community
